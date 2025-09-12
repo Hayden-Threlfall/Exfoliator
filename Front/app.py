41b828a59ca8aed708558a51e766605c3814a79a
acholthitchanta <78499478+acholthitchanta@users.noreply.github.com>
2025-09-12 18:13:21 -0500

    ok

--- Front/app.py
+++ Front/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 import socket
@@ -515,7 +515,7 @@ def parse_json_status(json_string):
 # Web Routes
 @app.route('/')
 def index():
-    return send_file('control.html')
+    return render_template('control.html')
 
 @app.route('/connect', methods=['POST'])
 def connect_machine():
